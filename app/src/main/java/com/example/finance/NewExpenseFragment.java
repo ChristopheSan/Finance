9f2159621b79bfe39a2a7304f1d9963c00301255
ChristopheSan <chrisjakesanchez@gmail.com>
2024-10-27 17:01:21 -0700

    Form UI completed - successfully created Expense. Needs DB connectivity.

--- app/src/main/java/com/example/finance/NewExpenseFragment.java
+++ app/src/main/java/com/example/finance/NewExpenseFragment.java
@@ -2,19 +2,24 @@ package com.example.finance;
 
 import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.HashMap;
@@ -23,11 +28,19 @@ import java.util.Map;
 
 
 public class NewExpenseFragment extends Fragment {
+    Button returnButton;
+
     Button datePickerButton;
     TextView dateText;
 
-    Button returnButton;
+    EditText descriptionEditText;
+    EditText amountEditText;
+    EditText vendorEditText;
+
     Spinner categorySpinner;
+
+    Button submitButton;
+    DatePickerDialog datePicker;
 
     ExpenseFragment parentFragment;
 
@@ -51,11 +64,15 @@ public class NewExpenseFragment extends Fragment {
             }
         });
 
+        // Initialize Edit Texts
+        descriptionEditText = view.findViewById(R.id.descriptionEditText);
+        amountEditText = view.findViewById(R.id.amountEditText);
+        vendorEditText = view.findViewById(R.id.VendorEditText);
 
         // Date Picker Button
         dateText = view.findViewById(R.id.displayDateTextView);
         Calendar calendar = Calendar.getInstance();
-        dateText.setText(((calendar.get(Calendar.MONTH) + 1) + "/" + calendar.get(Calendar.DAY_OF_MONTH) + "/" + calendar.get(Calendar.YEAR)));
+        dateText.setText(((calendar.get(Calendar.MONTH) + 1) + "-" + calendar.get(Calendar.DAY_OF_MONTH) + "-" + calendar.get(Calendar.YEAR)));
 
         datePickerButton = view.findViewById(R.id.date_picker_button);
         datePickerButton.setOnClickListener(new View.OnClickListener() {
@@ -68,7 +85,84 @@ public class NewExpenseFragment extends Fragment {
         // Initialize Category Spinner
         initCategorySpinner(view);
 
+        // initialize submit button
+        submitButton = view.findViewById(R.id.submitExpenseButton);
+        submitButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                buildAndSubmitExpense();
+            }
+        });
+
         return view;
+    }
+
+    private void buildAndSubmitExpense() {
+        Expense expense = new Expense();
+        //
+
+        LocalDate date = LocalDate.parse(dateText.getText().toString(), DateTimeFormatter.ofPattern("MM-dd-yyyy"));
+        Log.d("LocalDate creation","Date: " + date);
+
+        String description = "";
+        if (descriptionEditText.getText().toString().equals("")) {
+            // toast message to enter a description
+            Toast.makeText(getContext(), "Please enter a description", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        else {
+            description = descriptionEditText.getText().toString();
+            Log.d("Description","Description: " + description);
+        }
+
+        double amount = 0;
+        if (amountEditText.getText().toString().equals("")) {
+            // toast message to enter an amount
+            Toast.makeText(getContext(), "Please enter an amount", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        else {
+            amount = Double.parseDouble(amountEditText.getText().toString());
+            Log.d("Amount","Amount: " + amount);
+        }
+
+        String vendor = "";
+        if (vendorEditText.getText().toString().equals("")) {
+            // toast message to enter a vendor
+            Toast.makeText(getContext(), "Please enter a vendor", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        else {
+            vendor = vendorEditText.getText().toString();
+            Log.d("Vendor","Vendor: " + vendor);
+        }
+
+        SpendingCategory category = SpendingCategory.valueOf(categorySpinner.getSelectedItem().toString());
+        Log.d("Category","Category: " + category);
+
+        Expense newExpense = new Expense(date, description, vendor, amount, category);
+        Log.d("New Expense", newExpense.toString());
+
+        sendToDB(newExpense);
+        resetTextFields();
+        Toast.makeText(getContext(), "New Expense Added", Toast.LENGTH_SHORT).show();
+
+
+    }
+
+    private void resetTextFields() {
+        descriptionEditText.setText("");
+        amountEditText.setText("");
+        vendorEditText.setText("");
+
+        Calendar calendar = Calendar.getInstance();
+        dateText.setText(((calendar.get(Calendar.MONTH) + 1) + "-" + calendar.get(Calendar.DAY_OF_MONTH) + "-" + calendar.get(Calendar.YEAR)));
+    }
+
+    private void sendToDB(Expense newExpense) {
+        // TODO: Send to DB
+
     }
 
     private void openDatePicker() {
@@ -81,7 +175,7 @@ public class NewExpenseFragment extends Fragment {
             @Override
             public void onDateSet(DatePicker datePicker, int year, int month, int day) {
                 month = month + 1;
-                dateText.setText((String.valueOf(month)) + "/" + String.valueOf(day) + "/" + String.valueOf(year));
+                dateText.setText((String.valueOf(month)) + "-" + String.valueOf(day) + "-" + String.valueOf(year));
             };
         }, year, month, day); // This is the initial date that is displayed/selected
 
