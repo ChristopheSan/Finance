6af1c9da3357d3eed918eae4ff72b3fd53b965a0
ChristopheSan <chrisjakesanchez@gmail.com>
2024-11-12 16:25:01 -0800

    Modified BudgetFragment to utilize the new view model approach

--- app/src/main/java/com/example/finance/BudgetFragment.java
+++ app/src/main/java/com/example/finance/BudgetFragment.java
@@ -8,12 +8,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 
 public class BudgetFragment extends Fragment {
     private UserBudget userBudget; // This holds the budget information for a single budget
+    private UserBudgetViewModel userBudgetViewModel;
     private RecyclerView budgetRecyclerView;
     private BudgetViewAdapter adapter;
 
@@ -34,22 +37,26 @@ public class BudgetFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_budget, container, false);
-        if (getArguments() != null) {
-            userBudget = getArguments().getParcelable("userBudget");
-        }
-        else {
-            userBudget = new UserBudget(0, "Default Budget"); // this shouldn't happen
-            // if there's an error at some point it's probably because no budgetItems
-            // TODO: handle this error better
-        }
+
+        // Initialize the ViewModel
+        userBudgetViewModel = new ViewModelProvider(requireActivity()).get(UserBudgetViewModel.class);
 
         budgetRecyclerView = view.findViewById(R.id.budgetRecyclerView);
         int numberOfGridColumns = 2;
         GridLayoutManager gridLayoutManager = new GridLayoutManager(this.getContext(), numberOfGridColumns);
         budgetRecyclerView.setLayoutManager(gridLayoutManager);
 
-        adapter = new BudgetViewAdapter(this.getContext(), userBudget.getBudgetItems());
+        adapter = new BudgetViewAdapter(this.getContext());
         budgetRecyclerView.setAdapter(adapter);
+
+        userBudgetViewModel.getBudget().observe(getViewLifecycleOwner(), new Observer<UserBudget>() {
+            @Override
+            public void onChanged(UserBudget userBudget) {
+                adapter.setBudgetItems(userBudget.getBudgetItems());
+                adapter.notifyDataSetChanged();
+            }
+        });
+
 
         // Inflate the layout for this fragment
         return view;
